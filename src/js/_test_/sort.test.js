845963f03539a8ed89d07026759b373396d40ff5
DenisKomov <143321805+DenisKomov@users.noreply.github.com>
2025-01-22 18:26:48 +0300

    commit now
    commit now

--- src/js/_test_/sort.test.js
+++ src/js/_test_/sort.test.js
@@ -1,31 +1,25 @@
-import sortUnits from '../sort';
+import { healthIndicator, sortCharacters } from '../app';
 
-test('sort tobe mistake', () => {
-    const a = [
-        { name: 'мечник', health: 10 },
-        { name: 'маг', health: 100 },
-        { name: 'лучник', health: 80 },
-    ];
-    const b = [
-        { name: 'маг', health: 100 },
-        { name: 'лучник', health: 80 },
-        { name: 'мечник', health: 10 },
-    ];
-    const result = sortUnits(a);
-    expect(result).not.toBe(b);
+test.each([
+    [{ name: 'char1', health: 90 }, 'healthy'],
+    [{ name: 'char2', health: 45 }, 'wounded'],
+    [{ name: 'char3', health: 5 }, 'critical'],
+])('test health status for %p', (object, expectedResult) => {
+    const result = healthIndicator(object);
+    expect(result).toBe(expectedResult);
 });
 
-test('sort toEqual success', () => {
-    const a = [
+test('test sorting feature', () => {
+    const objectList = [
         { name: 'мечник', health: 10 },
         { name: 'маг', health: 100 },
         { name: 'лучник', health: 80 },
     ];
-    const b = [
+    const expectedResult = [
         { name: 'маг', health: 100 },
         { name: 'лучник', health: 80 },
         { name: 'мечник', health: 10 },
     ];
-    const result = sortUnits(a);
-    expect(result).toEqual(b);
+    const result = sortCharacters(objectList);
+    expect(result).toEqual(expectedResult);
 });
