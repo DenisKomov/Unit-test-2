d583cc343e98ad1f233186433b299fb44490b835
DenisKomov <143321805+DenisKomov@users.noreply.github.com>
2025-01-22 18:27:32 +0300

    commit now 2
    commit now 2

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,12 @@
-import sortUnits from './sort';
+export function healthIndicator(object) {
+    if (object.health > 50) {
+        return 'healthy';
+    } if (object.health > 15) {
+        return 'wounded';
+    }
+    return 'critical';
+}
 
-const a = [
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-];
-sortUnits(a);
+export function sortCharacters(array) {
+    return array.sort((x, y) => y.health - x.health);
+}
